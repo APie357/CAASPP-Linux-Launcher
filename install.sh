aa3f514fbd152dab8426235054401cbb3495ba84
Genius Format <71953979+Genius-Format@users.noreply.github.com>
2021-04-15 09:35:32 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo Installing...
+cd ~
+wget https://files.portal.cambiumast.com/california/SecureBrowsers/SB2020/CASecureBrowser12.5-2020-06-17-x86_64.tar.bz2
+tar xfjv CASecureBrowser12.5-2020-06-17-x86_64.tar.bz2
+cd CASecureBrowser
+./install-icon.sh
+Installed Sucessfully!
+Run the command below to start the browser:
+~/CASecureBrowser/
